990eeb7d9b205b91fcda86e83dfbce9046102c88
abhaysaqi <abhaysaqi9@gmail.com>
2026-01-10 18:02:55 +0530

    done example

---
+++ example/apptexts.dart
@@ -0,0 +1,5 @@
+class AppTexts {
+  static const String loginButton = "Sign In";
+  static const String welcomeMsg = "Welcome to our application!";
+  static const String errorGeneric = "Something went wrong.";
+}
